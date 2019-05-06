0396262fcf2685e2c48abed7f13365e163b6e3a7
Yuta Aoki <nightrage.metal66@gmail.com>
2019-05-06 11:18:17 +0900

    modified recruit lists to change

--- wp-content/themes/gensen_tcd050/index.php
+++ wp-content/themes/gensen_tcd050/index.php
@@ -5,21 +5,23 @@
     // タグフィルター用ターム配列
   $tags = false;
   if ($custom_search_vars) {
-    if ($dp_options['show_search_results_tag_filter']) {
-      if ($dp_options['searcn_post_type'] == 'post') {
-        $tags = get_terms('post_tag', array());
-      } elseif ($dp_options['searcn_post_type'] == 'introduce') {
-        $tags = get_terms($dp_options['introduce_tag_slug'], array());
+      if ($dp_options['show_search_results_tag_filter']) {
+          if ($dp_options['searcn_post_type'] == 'post') {
+              $tags = get_terms('post_tag', array());
+          } elseif ($dp_options['searcn_post_type'] == 'introduce') {
+              $tags = get_terms($dp_options['introduce_tag_slug'], array());
+          }
+          if (!$tags || is_wp_error($tags)) {
+              $tags = false;
+          }
       }
-      if (!$tags || is_wp_error($tags)) $tags = false;
-    }
   }
 
   // sort
   if (!empty($_REQUEST['sort']) && in_array($_REQUEST['sort'], array('date_asc', 'date_desc', 'views'))) {
-    $sort = $_REQUEST['sort'];
+      $sort = $_REQUEST['sort'];
   } else {
-    $sort = 'date_desc';
+      $sort = 'date_desc';
   }
   $sort_base_url = remove_query_arg('sort');
   $sort_base_url = preg_replace('#/page/\d+#', '', $sort_base_url);
@@ -28,178 +30,249 @@
 <?php get_template_part('breadcrumb'); ?>
 
 <div class="archive_header">
- <div class="inner">
+  <div class="inner">
 
-<?php if (is_category()) { ?>
- <h2 id="archive_headline" class="headline rich_font"><?php echo single_cat_title('', false); ?>のリゾートバイト求人</h2>
-<?php } elseif (is_tag() || is_tax($dp_options['introduce_tag_slug'])) { ?>
- <h2 id="archive_headline" class="headline rich_font"><?php echo single_tag_title('', false); ?>のリゾートバイト求人</h2>
- <?php } elseif (is_search()) { ?>
- <?php if ( have_posts() ) : ?>
- <h2 id="archive_headline" class="headline rich_font"><?php printf(__('Search results for %s', 'tcd-w'), get_search_query()); ?></h2>
- <?php else: ?>
- <h2 id="archive_headline" class="headline rich_font"><?php _e('Search results','tcd-w'); ?></h2>
-<?php endif; ?>
+    <?php if (is_category()) {
+    ?>
+    <h2 id="archive_headline" class="headline rich_font"><?php echo single_cat_title('', false); ?>のリゾートバイト求人
+    </h2>
+    <?php
+} elseif (is_tag() || is_tax($dp_options['introduce_tag_slug'])) {
+        ?>
+    <h2 id="archive_headline" class="headline rich_font"><?php echo single_tag_title('', false); ?>のリゾートバイト求人
+    </h2>
+    <?php
+    } elseif (is_search()) {
+        ?>
+    <?php if (have_posts()) : ?>
+    <h2 id="archive_headline" class="headline rich_font"><?php printf(__('Search results for %s', 'tcd-w'), get_search_query()); ?>
+    </h2>
+    <?php else: ?>
+    <h2 id="archive_headline" class="headline rich_font"><?php _e('Search results', 'tcd-w'); ?>
+    </h2>
+    <?php endif; ?>
 
-<?php } elseif (is_day()) { ?>
- <h2 id="archive_headline" class="headline rich_font"><?php printf(__('Archive for &#8216; %s &#8217;', 'tcd-w'), get_the_time(__('F jS, Y', 'tcd-w'))); ?></h2>
+    <?php
+    } elseif (is_day()) {
+        ?>
+    <h2 id="archive_headline" class="headline rich_font"><?php printf(__('Archive for &#8216; %s &#8217;', 'tcd-w'), get_the_time(__('F jS, Y', 'tcd-w'))); ?>
+    </h2>
 
-<?php } elseif (is_month()) { ?>
- <h2 id="archive_headline" class="headline rich_font"><?php printf(__('Archive for &#8216; %s &#8217;', 'tcd-w'), get_the_time(__('F, Y', 'tcd-w'))); ?>aaa</h2>
+    <?php
+    } elseif (is_month()) {
+        ?>
+    <h2 id="archive_headline" class="headline rich_font"><?php printf(__('Archive for &#8216; %s &#8217;', 'tcd-w'), get_the_time(__('F, Y', 'tcd-w'))); ?>aaa
+    </h2>
 
-<?php } elseif (is_year()) { ?>
- <h2 id="archive_headline" class="headline rich_font"><?php printf(__('Archive for &#8216; %s &#8217;', 'tcd-w'), get_the_time(__('Y', 'tcd-w'))); ?></h2>
+    <?php
+    } elseif (is_year()) {
+        ?>
+    <h2 id="archive_headline" class="headline rich_font"><?php printf(__('Archive for &#8216; %s &#8217;', 'tcd-w'), get_the_time(__('Y', 'tcd-w'))); ?>
+    </h2>
 
-<?php } elseif (is_author()) { ?>
-<?php global $wp_query; $curauth = $wp_query->get_queried_object(); //get the author info ?>
- <h2 id="archive_headline" class="headline rich_font"><?php printf(__('Archive for the &#8216; %s &#8217;', 'tcd-w'), $curauth->display_name ); ?></h2>
+    <?php
+    } elseif (is_author()) {
+        ?>
+    <?php global $wp_query;
+        $curauth = $wp_query->get_queried_object(); //get the author info?>
+    <h2 id="archive_headline" class="headline rich_font"><?php printf(__('Archive for the &#8216; %s &#8217;', 'tcd-w'), $curauth->display_name); ?>
+    </h2>
 
-<?php } elseif (is_home()) { ?>
- <h2 id="archive_headline" class="headline rich_font"><?php echo esc_html($dp_options['blog_archive_headline']); ?></h2>
-<?php if ($dp_options['blog_archive_desc']) { ?>
- <p id="archive_desc"><?php echo str_replace(array("\r\n", "\r", "\n"), '<br>', esc_html($dp_options['blog_archive_desc'])); ?></p>
-<?php } ?>
+    <?php
+    } elseif (is_home()) {
+        ?>
+    <h2 id="archive_headline" class="headline rich_font"><?php echo esc_html($dp_options['blog_archive_headline']); ?>
+    </h2>
+    <?php if ($dp_options['blog_archive_desc']) {
+            ?>
+    <p id="archive_desc"><?php echo str_replace(array("\r\n", "\r", "\n"), '<br>', esc_html($dp_options['blog_archive_desc'])); ?>
+    </p>
+    <?php
+        } ?>
 
-<?php } ?>
- </div>
+    <?php
+    } ?>
+  </div>
 </div>
-<div id="main_col" class="clearfix">  
- <div id="left_col" class="custom_search_results">
+<div id="main_col" class="clearfix">
+  <div id="left_col" class="custom_search_results">
 
-<?php if ( have_posts() ) : ?>
- <dl class="archive_sort clearfix">
-  <dt><?php _e('Sort condition', 'tcd-w'); ?></dt>
-  <dd><a href="<?php echo esc_attr(add_query_arg('sort', 'date_desc', $sort_base_url)); ?>"<?php if ($sort == 'date_desc') echo ' class="active"'; ?>><?php _e('Newest first', 'tcd-w'); ?></a></dd>
-  <dd><a href="<?php echo esc_attr(add_query_arg('sort', 'date_asc', $sort_base_url)); ?>"<?php if ($sort == 'date_asc') echo ' class="active"'; ?>><?php _e('Oldest first', 'tcd-w'); ?></a></dd>
-  <dd><a href="<?php echo esc_attr(add_query_arg('sort', 'views', $sort_base_url)); ?>"<?php if ($sort == 'views') echo ' class="active"'; ?>><?php _e('Large number of views', 'tcd-w'); ?></a></dd>
- </dl>
+    <?php if (have_posts()) : ?>
+    <dl class="archive_sort clearfix">
+      <dt><?php _e('Sort condition', 'tcd-w'); ?>
+      </dt>
+      <dd><a
+          href="<?php echo esc_attr(add_query_arg('sort', 'date_desc', $sort_base_url)); ?>"
+          <?php if ($sort == 'date_desc') {
+        echo ' class="active"';
+    } ?>><?php _e('Newest first', 'tcd-w'); ?></a>
+      </dd>
+      <dd><a
+          href="<?php echo esc_attr(add_query_arg('sort', 'date_asc', $sort_base_url)); ?>"
+          <?php if ($sort == 'date_asc') {
+        echo ' class="active"';
+    } ?>><?php _e('Oldest first', 'tcd-w'); ?></a>
+      </dd>
+      <dd><a
+          href="<?php echo esc_attr(add_query_arg('sort', 'views', $sort_base_url)); ?>"
+          <?php if ($sort == 'views') {
+        echo ' class="active"';
+    } ?>><?php _e('Large number of views', 'tcd-w'); ?></a>
+      </dd>
+    </dl>
 
-<?php get_template_part('navigation2'); ?>
+    <?php get_template_part('navigation2'); ?>
 
- <ol id="post_list2">
+    <ol id="post_list2">
 
-<?php   while ( have_posts() ) : the_post(); ?>
-  <li class="article">
-   <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>" class="clearfix">
-    <div class="r_score" id=score<?php echo resorn_score(); ?> >
-      <div class="r_score_title">RESORNスコア</div>
-      <div class="r_score_text" id="score<?php echo resorn_score(); ?>"><?php echo resorn_score(); ?></div>
-    <!-- <div class="image">
+      <?php   while (have_posts()) : the_post(); ?>
+      <li class="article">
+        <a href="<?php the_permalink() ?>"
+          title="<?php the_title_attribute(); ?>" class="clearfix">
+          <div class="r_score" id=score<?php echo resorn_score(); ?>
+            >
+            <div class="r_score_title">RESORNスコア</div>
+            <div class="r_score_text"
+              id="score<?php echo esc_html($post->resorn_score_field); ?>">
+              <?php echo esc_html($post->resorn_score_field); ?>
+            </div>
+            <!-- <div class="image">
       <?php /*
       if ( has_post_thumbnail() ) {
-          the_post_thumbnail(); 
-      } else { 
+          the_post_thumbnail();
+      } else {
         if ( is_mobile() ) {
           echo '<img src="' . get_template_directory_uri() . '/img/common/no_image1.gif" title="" alt="" />';
         } else {
           echo '<img src="' . get_template_directory_uri() . '/img/common/no_image2.gif" title="" alt="" />';
         }
       } */?>-->
-    </div>
-    <div class="info">
-<?php
+          </div>
+          <div class="info">
+            <?php
         $metas = array();
         if ($post->post_type == 'post') {
-          if ($dp_options['show_categories']) {
-            foreach(explode('-', $dp_options['show_categories']) as $cat) {
-              if ($cat == 1) {
-                $terms = get_the_terms($post->ID, 'category');
-                if ($terms && !is_wp_error($terms)) {
-                  foreach ($terms as $term) {
-                    $metas['category'][] = '<span class="cat-category" data-href="'.get_term_link($term).'" title="'.esc_attr($term->name).'">'.esc_html($term->name).'</span>';
-                  }
-                  $metas['category'] = '<li class="cat">'.implode('', $metas['category']).'</li>';
+            if ($dp_options['show_categories']) {
+                foreach (explode('-', $dp_options['show_categories']) as $cat) {
+                    if ($cat == 1) {
+                        $terms = get_the_terms($post->ID, 'category');
+                        if ($terms && !is_wp_error($terms)) {
+                            foreach ($terms as $term) {
+                                $metas['category'][] = '<span class="cat-category" data-href="'.get_term_link($term).'" title="'.esc_attr($term->name).'">'.esc_html($term->name).'</span>';
+                            }
+                            $metas['category'] = '<li class="cat">'.implode('', $metas['category']).'</li>';
+                        }
+                    } elseif (!empty($dp_options['use_category'.$cat])) {
+                        $terms = get_the_terms($post->ID, $dp_options['category'.$cat.'_slug']);
+                        if ($terms && !is_wp_error($terms)) {
+                            foreach ($terms as $term) {
+                                $metas['category'.$cat][] = '<span class="cat-'.esc_attr($dp_options['category'.$cat.'_slug']).'" data-href="'.get_term_link($term).'" title="'.esc_attr($term->name).'">'.esc_html($term->name).'</span>';
+                            }
+                            $metas['category'.$cat] = '<li class="cat">'.implode('', $metas['category'.$cat]).'</li>';
+                        }
+                    }
                 }
-              } elseif (!empty($dp_options['use_category'.$cat])) {
-                $terms = get_the_terms($post->ID, $dp_options['category'.$cat.'_slug']);
-                if ($terms && !is_wp_error($terms)) {
-                  foreach ($terms as $term) {
-                    $metas['category'.$cat][] = '<span class="cat-'.esc_attr($dp_options['category'.$cat.'_slug']).'" data-href="'.get_term_link($term).'" title="'.esc_attr($term->name).'">'.esc_html($term->name).'</span>';
-                  }
-                  $metas['category'.$cat] = '<li class="cat">'.implode('', $metas['category'.$cat]).'</li>';
-                }
-              }
             }
-          }
-
         } elseif ($post->post_type == $dp_options['introduce_slug']) {
-          if ($dp_options['show_introduce_categories']) {
-            foreach(explode('-', $dp_options['show_introduce_categories']) as $cat) {
-              if (!empty($dp_options['use_introduce_category'.$cat])) {
-                $terms = get_the_terms($post->ID, $dp_options['introduce_category'.$cat.'_slug']);
-                if ($terms && !is_wp_error($terms)) {
-                  $term = array_shift($terms);
-                  $metas[] = '<li class="cat"><span class="cat-'.esc_attr($dp_options['introduce_category'.$cat.'_slug']).'" data-href="'.get_term_link($term).'" title="'.esc_attr($term->name).'">'.esc_html($term->name).'</span></li>';
+            if ($dp_options['show_introduce_categories']) {
+                foreach (explode('-', $dp_options['show_introduce_categories']) as $cat) {
+                    if (!empty($dp_options['use_introduce_category'.$cat])) {
+                        $terms = get_the_terms($post->ID, $dp_options['introduce_category'.$cat.'_slug']);
+                        if ($terms && !is_wp_error($terms)) {
+                            $term = array_shift($terms);
+                            $metas[] = '<li class="cat"><span class="cat-'.esc_attr($dp_options['introduce_category'.$cat.'_slug']).'" data-href="'.get_term_link($term).'" title="'.esc_attr($term->name).'">'.esc_html($term->name).'</span></li>';
+                        }
+                    }
                 }
-              }
             }
-          }
         }
 
         if ($metas) {
-          echo '    <ul class="meta clearfix">'.implode('', $metas).'</ul>'."\n";
+            echo '    <ul class="meta clearfix">'.implode('', $metas).'</ul>'."\n";
         }
 ?>
-     <h3 class="title"><?php trim_title(38); ?></h3>
-     <?php
-        if ( is_mobile() ){
-          if ( has_post_thumbnail() ) {
-            echo '<div class="image">';
-            the_post_thumbnail();
-            echo '</div>';
-          }
+            <h3 class="title"><?php trim_title(38); ?>
+            </h3>
+            <?php
+        if (is_mobile()) {
+            if (has_post_thumbnail()) {
+                echo '<div class="image">';
+                the_post_thumbnail();
+                echo '</div>';
+            }
         }
      ?>
-     <div class="r_score_sp" id=score<?php echo resorn_score(); ?> >
-      <div class="r_score_title_sp">RESORNスコア</div>
-      <div class="r_score_text_sp"><span class="score<?php echo resorn_score(); ?>"><?php echo resorn_score(); ?></span> （最高は10）</div>
-     </div>
-     <table class="recruit_list">
-      <tr>
-       <th>会社</th>
-       <td><img class="recruit_list_company" src="<?php echo do_shortcode('[company_image]'); ?>"></td>
-      </tr>
-      <tr>
-       <th>職種</th>
-       <td><?php echo esc_html( $post->occupation_field ); ?></td>
-      </tr>
-      <tr>
-       <th>給与</th>
-       <td><?php echo wp_strip_all_tags(salary_fi()); ?></td>
-      </tr>
-     </table>
-     <div class="navigation2_icons">
-       <img class="<?php echo esc_html( $post->icon_dormitory_field ); ?>" src="https://resorn.net/wp-content/uploads/2018/02/new-koshitsuryo-1.png">
-       <img class="<?php echo esc_html( $post->icon_highIncome_field ); ?>" src="https://resorn.net/wp-content/uploads/2018/02/new-koshunyu-1.png">
-       <img class="<?php echo esc_html( $post->icon_campaign_field ); ?>" src="https://resorn.net/wp-content/uploads/2018/02/new-campaign-1.png">
-       <img class="<?php echo esc_html( $post->icon_meal_field ); ?>" src="https://resorn.net/wp-content/uploads/2018/02/new-shokuji-1.png">
-       <img class="<?php echo esc_html( $post->icon_transportationFee_field ); ?>" src="https://resorn.net/wp-content/uploads/2018/02/new-carloan-1.png">
-       <img class="<?php echo esc_html( $post->icon_wifi_field ); ?>" src="https://resorn.net/wp-content/uploads/2018/02/new-wifi-1.png">
-       <img class="<?php echo esc_html( $post->icon_spa_field ); ?>" src="https://resorn.net/wp-content/uploads/2018/02/new-spa-1.png">
-     </div>
-     <div class="recruit_list_btn">詳しい内容を見る</div>
-    </div>
-   </a>
-  </li>
-<?php   endwhile; ?>
+            <div class="r_score_sp" id=score<?php echo resorn_score(); ?> >
+              <div class="r_score_title_sp">RESORNスコア</div>
+              <div class="r_score_text_sp"><span
+                  class="score<?php echo esc_html($post->resorn_score_field); ?>"><?php echo esc_html($post->resorn_score_field); ?></span>
+                （最高は10）</div>
+            </div>
+            <table class="recruit_list">
+              <tr>
+                <th>会社</th>
+                <td><img class="recruit_list_company"
+                    src="<?php echo do_shortcode('[company_image]'); ?>">
+                </td>
+              </tr>
+              <tr>
+                <th>職種</th>
+                <td><?php echo esc_html($post->occupation_field); ?>
+                </td>
+              </tr>
+              <tr>
+                <th>給与</th>
+                <td><?php echo wp_strip_all_tags(salary_fi()); ?>
+                </td>
+              </tr>
+            </table>
+            <div class="navigation2_icons">
+              <img
+                class="<?php echo esc_html($post->icon_dormitory_field); ?>"
+                src="https://resorn.net/wp-content/uploads/2018/02/new-koshitsuryo-1.png">
+              <img
+                class="<?php echo esc_html($post->icon_highIncome_field); ?>"
+                src="https://resorn.net/wp-content/uploads/2018/02/new-koshunyu-1.png">
+              <img
+                class="<?php echo esc_html($post->icon_campaign_field); ?>"
+                src="https://resorn.net/wp-content/uploads/2018/02/new-campaign-1.png">
+              <img
+                class="<?php echo esc_html($post->icon_meal_field); ?>"
+                src="https://resorn.net/wp-content/uploads/2018/02/new-shokuji-1.png">
+              <img
+                class="<?php echo esc_html($post->icon_transportationFee_field); ?>"
+                src="https://resorn.net/wp-content/uploads/2018/02/new-carloan-1.png">
+              <img
+                class="<?php echo esc_html($post->icon_wifi_field); ?>"
+                src="https://resorn.net/wp-content/uploads/2018/02/new-wifi-1.png">
+              <img
+                class="<?php echo esc_html($post->icon_spa_field); ?>"
+                src="https://resorn.net/wp-content/uploads/2018/02/new-spa-1.png">
+            </div>
+            <div class="recruit_list_btn">詳しい内容を見る</div>
+          </div>
+        </a>
+      </li>
+      <?php   endwhile; ?>
 
- </ol><!-- END #post_list -->
-<?php else: ?>
- <p class="no_post"><?php _e('There is no registered post.', 'tcd-w'); ?></p>
-<?php endif; ?>
+    </ol><!-- END #post_list -->
+    <?php else: ?>
+    <p class="no_post"><?php _e('There is no registered post.', 'tcd-w'); ?>
+    </p>
+    <?php endif; ?>
 
-<?php get_template_part('navigation2'); ?>
+    <?php get_template_part('navigation2'); ?>
 
-<?php 
+    <?php
 $queried_object = get_queried_object();
-  if (get_template_part('tags-article/article', $queried_object->slug) ==TRUE ) {
-    get_template_part('tags-article/article', $queried_object->slug);
-} ?>
+  if (get_template_part('tags-article/article', $queried_object->slug) ==true) {
+      get_template_part('tags-article/article', $queried_object->slug);
+  } ?>
 
-</div><!-- END #left_col -->
+  </div><!-- END #left_col -->
 
-<?php get_sidebar(); ?>
+  <?php get_sidebar(); ?>
 
 </div><!-- END #main_col -->
 
-<?php get_footer(); ?>
+<?php get_footer();
