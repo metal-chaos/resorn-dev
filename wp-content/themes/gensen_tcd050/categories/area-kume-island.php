2cfe5c905bc6ae576a01454841c4859d532d8af7
Yuta Aoki <nightrage.metal66@gmail.com>
2019-07-31 14:35:09 +0900

    added content to kume-island

---
+++ wp-content/themes/gensen_tcd050/categories/area-kume-island.php
@@ -0,0 +1,75 @@
+<?php
+    $queried_object = get_queried_object();
+    $upload_dir = wp_upload_dir();
+?>
+</div>
+<img class="archive_heading_banner"
+    src="<?php  echo $upload_dir['baseurl']; ?>/categories/kyusyu/okinawa/kume-island/kume-island.jpg"
+    alt="<?php  echo $queried_object -> name; ?>の風景">
+<div class="inner">
+    <div class="archive_heading_wrap">
+        <p>久米島は島尻郡久米島町に属する面積約60㎢、人口約9,700人の島であり、沖縄本島の西方約100kmに位置しています。南西諸島の東シナ海側の火山列島の一つになっており、島全体が輝石安山岩で形成されています。島の周囲にはサンゴ礁が広がり、サトウキビやタバコ、パイナップルなどが生産されます。古く琉球王府の時代には、中国との交易の中継地として栄えました。島のほぼ全域と周辺海域が久米島県立自然公園に指定されています。日本の渚百選に選ばれた「イーフビーチ」や、高さ40メートルの巨岩「タチジャミ」などの観光スポットがあります。プロ野球チーム楽天のキャンプ地としても知られています。また、宇江城岳周辺の渓流や湿地にはキクザトサワヘビやクメジマボタルなどの希少水生生物が数多く生息しています。2008年には、国際的に重要な湿地を保全するラムサール条約に登録されています。
+        </p>
+    </div>
+    <h3 class="midashi"><?php echo esc_html($queried_object->name); ?>へのアクセス・所要時間</h3>
+    <table class="archive_heading_table">
+        <tr>
+            <th class="r1">出発地</th>
+            <th class="r2">手段</th>
+            <th class="r3">所要時間</th>
+        </tr>
+        <tr>
+            <td class="r1">東京（羽田）→久米島</td>
+            <td class="r2">飛行機</td>
+            <td class="r3">2時間35分</td>
+        </tr>
+        <tr>
+            <td class="r1">札幌（新千歳）→久米島</td>
+            <td class="r2">飛行機（那覇空港経由）</td>
+            <td class="r3">5時間45分</td>
+        </tr>
+        <tr>
+            <td class="r1">仙台（仙台空港）→久米島</td>
+            <td class="r2">飛行機（那覇空港経由）</td>
+            <td class="r3">4時間30分</td>
+        </tr>
+        <tr>
+            <td class="r1">名古屋（中部国際空港）→久米島</td>
+            <td class="r2">飛行機（那覇空港経由）</td>
+            <td class="r3">5時間</td>
+        </tr>
+        <tr>
+            <td class="r1">大阪（関空）→久米島</td>
+            <td class="r2">飛行機（那覇空港経由）</td>
+            <td class="r3">3時間35分</td>
+        </tr>
+        <tr>
+            <td class="r1">福岡（福岡空港）→久米島</td>
+            <td class="r2">飛行機（那覇空港経由）</td>
+            <td class="r3">4時間</td>
+        </tr>
+    </table>
+    <h3 class="midashi"><?php echo esc_html($queried_object->name); ?>の観光名所</h3>
+    <div class="archive_row">
+        <div class="archive_col archive_col-3">
+            <h4>イーフビーチ</h4>
+            <img src="<?php  echo $upload_dir['baseurl']; ?>/categories/kyusyu/okinawa/kume-island/eaf-beach.jpg"
+                alt="イーフビーチ">
+            <p>日本の渚100選にも選ばれた久米島を代表する美しいビーチです。はるか2km先まできめ細かい真っ白な砂で覆われた浜が広がり、海を見ているだけで時間の経つのを忘れます。波打ち際で小さな魚の泳いでいる姿を見ることができます。なお、砂浜が沖合いまで続く遠浅になっているため、干潮になると沖まで歩いて行けます。波は常に穏やかで、側には簡易シャワーやトイレが備わっており、海水浴には最適な環境になっています。朝陽を浴びて白く輝く砂浜を散歩したり、波の音を聴きながら夜空に煌めく星を眺めたり、若いカップルには絶景のデートスポットでもあります。
+            </p>
+        </div>
+        <div class="archive_col archive_col-3">
+            <h4>宇江城（うえぐすく）城跡</h4>
+            <img src="<?php  echo $upload_dir['baseurl']; ?>/categories/kyusyu/okinawa/kume-island/uegusuku-castle.jpg"
+                alt="宇江城（うえぐすく）城跡">
+            <p>久米島の北部にある標高310mの宇江城岳山頂に城跡が残っています。沖縄県内で一番標高の高い位置にあるため、久米島全景のパノラマを一望できる沖縄県内でも屈指の絶景スポットです。城跡からは青磁などの中国製陶磁器が数多く出土しており、久米島が中国貿易の中継地として繁栄していたことを物語っています。沖縄におけるグスク時代の政治・文化の様相を知る上で貴重な史跡であり、2009年（平成21年）に国の史跡に指定されました。記録がないため、築城の年代は不明ですが、久米中城（くめなかぐす）按司が築いたとされ、1510年に琉球統一の過程で滅ぼされました。城壁の石積みには珊瑚石灰岩が見られる他、周辺で採れる輝石安山岩が使用されています。
+            </p>
+        </div>
+        <div class="archive_col archive_col-3">
+            <h4>比屋定（ひやじょう）バンタ</h4>
+            <img src="<?php  echo $upload_dir['baseurl']; ?>/categories/kyusyu/okinawa/kume-island/hiyajo-banta.jpg"
+                alt="比屋定（ひやじょう）バンタ">
+            <p>島の東北部に位置する海抜200ｍの断崖絶壁にある人気の展望台です。「バンタ」という言葉は断崖絶壁を表しています。展望台に着くと、大きなシーサーが出迎えてくれます。展望台の２階に上がると、サンゴ礁の海岸線を始め、蒼色に染められた大海原を一望することができます。遠くを見ると、ハテの浜から渡名喜島、さらには慶良間諸島まで見渡せます。展望台の床には眺望説明図が描かれておりおり、方向を見定めながら目標となる景色を確認できます。また、絶壁の真下にある海岸には車海老の養殖場や「熱帯魚の家」と呼ばれる観光スポットもあります。きれいな景色を見ながら、展望台でのランチも乙なものです。
+            </p>
+        </div>
+    </div>
